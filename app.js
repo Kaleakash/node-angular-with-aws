7625c3f3da364dc4959197e43da39ac4c4e59c50
kaleakash <akash300383@gmail.com>
2021-04-25 16:53:32 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let express = require("express")
 let app = express();
 
-app.use(express.static(process.cwd()+"/angular-demo/dist/angular-demo"));
+app.use(express.static(process.cwd()));
 
 app.get("/hello",(req,res)=> {
 	res.send("Welcome")
 })
 
 app.get('/', (req,res) => {
- res.sendFile(process.cwd()+"/angular-demo/dist/angular-demo/index.html")
+ res.sendFile(__dirname+"/index.html")
 });
 app.get("/userInfo",(req,res)=> {
 res.json({"id":100,"name":"Ravi"})
